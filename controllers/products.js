41608a828bd7bbdd1144dca8e4f9fe37c9fc5c0f
Aashish Kaushik <aashishkaushik_bca18_21@its.edu.in>
2023-05-25 11:08:23 +0530

    add sorting feature in API

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,32 @@ exports.getAllProductsStatic = async (req, res) => {
 };
 exports.getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find(req.query);
+    const { name, price, featured, creaedAt, company, sort, field } = req.query;
+    query = {};
+    if (name) {
+      query.name = name;
+    }
+    // if (price) {
+    // }
+    if (featured) {
+      query.featured = featured;
+    }
+    if (company) {
+      query.company = company;
+    }
+    let result = Product.find(query);
+    if (sort) {
+      const sortValue = sort.split(',').join(' ');
+      
+      result = result.sort(sortValue);
+    } else {
+      result = result.sort('creaedAt');
+    }
+    if (field) {
+      const fieldValue = field.split(',').join(' ');
+      result = result.select(fieldValue);
+    }
+    const products = await result;
     res.status(200).json({ ProductCount: products.length, data: products });
   } catch (error) {
     res.status(404).json({ message: error.message });
